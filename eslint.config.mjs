fb251b8f4ab4be8e59c300cb12818af80ea18c1d
Eldor <eldor@protonmail.com>
2025-07-05 23:08:21 +0300

    feat: eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+import { globalIgnores } from 'eslint/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  globalIgnores([
+    '.next/*',
+    '.open-next/*',
+    '.source/*',
+    '.wrangler/*',
+  ]),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+];
+
+export default eslintConfig;
