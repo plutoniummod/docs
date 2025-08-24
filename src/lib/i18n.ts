e674a4389de072b809e4fab5aef166f811bad6ed
Eldor <eldor@protonmail.com>
2025-08-24 20:30:26 +0300

    feat: add lucide icons

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -3,5 +3,6 @@ import type { I18nConfig } from 'fumadocs-core/i18n';
 export const i18n: I18nConfig = {
   defaultLanguage: 'en',
   languages: ['en', 'de', 'es', 'fr'],
+  hideLocale: 'never',
   // hideLocale: 'default-locale',
 };
--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,11 +1,17 @@
 import { docs } from '@/.source';
 import { i18n } from '@/lib/i18n';
 import { loader } from 'fumadocs-core/source';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
-  // it assigns a URL to your pages
   baseUrl: '/',
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (icon && icon in icons) {
+      return createElement(icons[icon as keyof typeof icons]);
+    }
+  },
   i18n,
 });
